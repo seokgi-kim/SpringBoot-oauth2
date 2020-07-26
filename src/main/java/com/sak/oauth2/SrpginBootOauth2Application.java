ed43ff78346e7606a9f55622aac7b035c9600f34
seokgi <seokgi4885@gmail.com>
2020-07-26 16:36:09 +0900

    Initial commit

---
+++ src/main/java/com/sak/oauth2/SrpginBootOauth2Application.java
@@ -0,0 +1,13 @@
+package com.sak.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpginBootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpginBootOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sak/oauth2/SrpginBootOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sak.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpginBootOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
